17f2b1dfd2c26fa7289e0cc47b6fa9eacb87caae
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2024-07-06 00:01:39 +0900

    ヘッダーとフッターを追加

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import styled from "styled-components";
+
+const HeaderComponent = styled.header`
+  height: 5vh;
+  background: #22cc22;
+  color: #fff;
+  border-top: 3px solid #0c6eb7;
+  border-bottom: solid 1px #eee;
+`;
+
+const Text = styled.h1`
+  font-size: 3rem;
+  color: #333;
+  padding: 0.5rem;
+`;
+
+const Header = () => (
+  <HeaderComponent>
+    <Text>グリーンバック動画合成</Text>
+  </HeaderComponent>
+);
+
+export default Header;
---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+export default Wrapper;
