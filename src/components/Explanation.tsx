689945ca7a9646be734b6e919dffc1c84868e7ec
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2024-07-06 00:02:17 +0900

    サイトの説明テキストを編集

--- src/components/Explanation.tsx
+++ src/components/Explanation.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 80%;
+  margin: auto;
+  padding: 2.5rem 0 2.5rem 0;
+`;
+
+const Text = styled.h2`
+  font-size: 1.5rem;
+`;
+
 const Explanation = () => (
-  <div>
-    <p>グリーンバックの動画と背景画像をアップロードして、合成を行うことができます。</p>
-  </div>
+  <Wrapper>
+    <Text>
+      グリーンバックの動画と背景画像を
+      <br />
+      アップロードして、 合成を行うことができます。
+    </Text>
+  </Wrapper>
 );
 
 export default Explanation;
