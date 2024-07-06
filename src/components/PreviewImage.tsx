3514d876b08f421b5e753716fc38313d2fbc07ac
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2024-07-06 12:53:39 +0900

    create preview components

---
+++ src/components/PreviewImage.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { Skeleton } from "@mui/material";
+import { FC, useEffect, useState } from "react";
+
+type PreviewProps = {
+  file: File | null;
+};
+
+const PreviewImage: FC<PreviewProps> = ({ file }) => {
+  const [url, setUrl] = useState<string>("");
+  const isLoading = file && !url;
+
+  useEffect(() => {
+    if (!file) return;
+
+    let reader: FileReader | null = new FileReader();
+    reader.onloadend = () => {
+      const res = reader!.result;
+      if (res && typeof res === "string") {
+        setUrl(res);
+      }
+    };
+    reader.readAsDataURL(file);
+
+    return () => {
+      reader = null;
+    };
+  }, [file]);
+
+  return file ? isLoading ? <Skeleton /> : <Image src={url} alt={file.name} width={300} height={300} /> : null;
+};
+
+export default PreviewImage;
---
+++ src/components/PreviewVideo.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import styled from "styled-components";
+import { Skeleton } from "@mui/material";
+import { FC, useEffect, useState } from "react";
+
+type PreviewProps = {
+  file: File | null;
+};
+
+const StyledVideo = styled.video`
+  margin: auto;
+  width: 80%;
+`;
+
+const PreviewVideo: FC<PreviewProps> = ({ file }) => {
+  const [url, setUrl] = useState<string>("");
+  const isLoading = file && !url;
+
+  useEffect(() => {
+    if (!file) return;
+
+    let reader: FileReader | null = new FileReader();
+    reader.onloadend = () => {
+      const res = reader!.result;
+      if (res && typeof res === "string") {
+        setUrl(res);
+      }
+    };
+    reader.readAsDataURL(file);
+
+    return () => {
+      reader = null;
+    };
+  }, [file]);
+
+  return file ? isLoading ? <Skeleton /> : <StyledVideo src={url} /> : null;
+};
+
+export default PreviewVideo;
