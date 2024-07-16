81073d064638181ea0a99cbd426d75079984d502
kuri884 <krc.kuribayashi@gmail.com>
2024-07-17 08:59:44 +0900

    IDトークンの作成を編集

--- src/utils/getIdTokenFromMetadataServer.ts
+++ src/utils/getIdTokenFromMetadataServer.ts
@@ -1,15 +1,16 @@
 import { GoogleAuth } from "google-auth-library";
 
 const getIdTokenFromMetadataServer = async () => {
-  const googleAuth = new GoogleAuth();
   const targetAudience = "https://chroma-key-front-spbb34bsma-dt.a.run.app";
-  // const URL = "https://chroma-key-api-spbb34bsma-dt.a.run.app";
+  const url = "https://chroma-key-api-spbb34bsma-dt.a.run.app";
+
+  const googleAuth = new GoogleAuth();
 
   const client = await googleAuth.getIdTokenClient(targetAudience);
 
-  const token = await client.idTokenProvider.fetchIdToken(targetAudience);
+  const res = await client.request({ url });
 
-  return token;
+  return res.data;
 };
 
 export default getIdTokenFromMetadataServer;
