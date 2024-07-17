0e2808d2540500db6484cc1c7e63636546a20497
kuri884 <krc.kuribayashi@gmail.com>
2024-07-17 15:40:06 +0900

    Authorizationの修正

--- src/utils/getIdTokenFromMetadataServer.ts
+++ src/utils/getIdTokenFromMetadataServer.ts
@@ -1,40 +1,16 @@
-import { JWT } from "google-auth-library";
+import { GoogleAuth } from "google-auth-library";
 
-const serviceAccountKey = JSON.parse(
-  Buffer.from(process.env.GOOGLE_CLOUD_SERVICE_ACCOUNT_KEY as string, "base64").toString(),
-);
+const getIdTokenFromMetadataServer = async () => {
+  const googleAuth = new GoogleAuth();
+  // const targetAudience = "https://chroma-key-front-spbb34bsma-dt.a.run.app";
+  const url = "https://chroma-key-api-spbb34bsma-dt.a.run.app/compose";
 
-const getGoogleCloudAccessToken = async () => {
-  const client = new JWT({
-    email: serviceAccountKey.client_email,
-    key: serviceAccountKey.private_key,
-    scopes: ["https://www.googleapis.com/auth/cloud-platform"],
-  });
+  // const client = await googleAuth.getIdTokenClient(targetAudience);
+  const client = await googleAuth.getIdTokenClient(url);
 
-  const url = "https://chroma-key-api-spbb34bsma-dt.a.run.app";
+  const headers = await client.getRequestHeaders();
 
-  const tokens = await client.fetchIdToken(url);
-  console.info(tokens);
-
-  return tokens;
+  return headers.Authorization;
 };
 
-// const getIdTokenFromMetadataServer = async () => {
-// const targetAudience = "https://chroma-key-front-spbb34bsma-dt.a.run.app";
-// const url = "https://chroma-key-api-spbb34bsma-dt.a.run.app/compose";
-
-// const client = await googleAuth.getIdTokenClient(targetAudience);
-// const client = await googleAuth.getIdTokenClient(url);
-
-// const res = await client.request({ url });
-// console.info(res.data);
-
-// return res.data;
-
-//   const token = await client.idTokenProvider.fetchIdToken(targetAudience);
-
-//   return token;
-// };
-
-// export default getIdTokenFromMetadataServer;
-export default getGoogleCloudAccessToken;
+export default getIdTokenFromMetadataServer;
