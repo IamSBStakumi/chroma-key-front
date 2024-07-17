aed52d8380fdb7caabc53d110343012d29a39752
kuri884 <krc.kuribayashi@gmail.com>
2024-07-17 09:45:46 +0900

    idトークンを取得したい

--- src/utils/getIdTokenFromMetadataServer.ts
+++ src/utils/getIdTokenFromMetadataServer.ts
@@ -1,16 +1,22 @@
 import { GoogleAuth } from "google-auth-library";
 
+const googleAuth = new GoogleAuth();
+
 const getIdTokenFromMetadataServer = async () => {
   const targetAudience = "https://chroma-key-front-spbb34bsma-dt.a.run.app";
-  const url = "https://chroma-key-api-spbb34bsma-dt.a.run.app";
-
-  const googleAuth = new GoogleAuth();
+  // const url = "https://chroma-key-api-spbb34bsma-dt.a.run.app/compose";
 
   const client = await googleAuth.getIdTokenClient(targetAudience);
+  // const client = await googleAuth.getIdTokenClient(url);
 
-  const res = await client.request({ url });
+  // const res = await client.request({ url });
+  // console.info(res.data);
 
-  return res.data;
+  // return res.data;
+
+  const token = await client.idTokenProvider.fetchIdToken(targetAudience);
+
+  return token;
 };
 
 export default getIdTokenFromMetadataServer;
