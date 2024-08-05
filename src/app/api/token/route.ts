d46073b91e06ef799311dbd8a0cf8d6e83ea256e
kuri884 <krc.kuribayashi@gmail.com>
2024-08-05 10:48:53 +0900

    getTokenAPI

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -1,17 +1,13 @@
-// import { GoogleAuth } from "google-auth-library";
-import { NextRequest, NextResponse } from "next/server";
+import { GoogleAuth } from "google-auth-library";
+import { NextResponse } from "next/server";
 
-export default async function handler(req: NextRequest) {
-  console.log(req.method);
-  if (req.method !== "GET") {
-    return NextResponse.json({ message: "Not Allowed Method" }, { status: 405 });
-  }
-  // const googleAuth = new GoogleAuth();
-  // const url = "https://chroma-key-api-spbb34bsma-dt.a.run.app";
+export async function GET() {
+  const googleAuth = new GoogleAuth();
+  const url = "https://chroma-key-api-spbb34bsma-dt.a.run.app";
 
-  // const client = await googleAuth.getIdTokenClient(url);
+  const client = await googleAuth.getIdTokenClient(url);
 
-  // const headers = await client.getRequestHeaders();
+  const headers = await client.getRequestHeaders();
 
-  return NextResponse.json({ message: "headers.Authorization" }, { status: 200 });
+  return NextResponse.json({ message: headers.Authorization }, { status: 200 });
 }
