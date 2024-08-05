2a222ef39331cbfe20cc6ce290ceee216359fa7a
kuri884 <krc.kuribayashi@gmail.com>
2024-08-05 13:01:34 +0900

    進捗度表示用のコンポーネントを作成

---
+++ src/components/ProgressComponent.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const ProgressComponent = () => {
+  const [token, setToken] = useState<string | undefined>();
+  const [progress, setProgress] = useState("0");
+
+  const getToken = async () => {
+    const response = await fetch(`/api/token`);
+    const result = await response.json();
+    setToken(result);
+  };
+  getToken();
+
+  useEffect(() => {
+    const ws = new WebSocket("wss://chroma-key-api-spbb34bsma-dt.a.run.app/ws");
+
+    ws.onopen = () => {
+      if (token) ws.send(token);
+    };
+
+    ws.onmessage = (event) => {
+      const e = JSON.parse(event.data);
+      setProgress(e.progress);
+    };
+
+    ws.onclose = () => {};
+
+    const interval = setInterval(() => {
+      if (ws.readyState === WebSocket.OPEN) {
+        ws.send("progress");
+      } else {
+        clearInterval(interval);
+      }
+    }, 5000);
+
+    return () => {
+      setProgress("0");
+      ws.close();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <h2> 進捗率: {progress}%</h2>;
+};
+
+export default ProgressComponent;
