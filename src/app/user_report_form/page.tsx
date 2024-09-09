15c11361e05c5e9cfed9bdbf4ec10831d87bde16
kuri884 <krc.kuribayashi@gmail.com>
2024-09-09 16:43:05 +0900

    仮に不具合報告ページを作成し、遷移できるように編集

--- src/app/user_report_form/page.tsx
+++ src/app/user_report_form/page.tsx
@@ -1,26 +1,49 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useMutation } from "@tanstack/react-query";
 import LoadingSpinner from "@/components/LoadingSpinner";
 
 const UserReportForm = () => {
   const [isPosted, setPosted] = useState(false);
+  const [message, setMessage] = useState("");
+  const titleRef = useRef<HTMLInputElement>(null);
+  const bodyRef = useRef<HTMLInputElement>(null);
+
   const postReport = async () => {
-    await fetch("/api/report", {
+    if (!titleRef.current || !bodyRef.current) return;
+    if (titleRef.current.value === "" || bodyRef.current.value === "") {
+      setMessage("タイトルまたは不具合内容が入力されていません");
+
+      return;
+    }
+
+    const response = await fetch("/api/report", {
       method: "POST",
-    });
+      body: JSON.stringify({ title: titleRef.current.value, body: bodyRef.current.value }),
+    })
+      .then(async (res) => await res.json())
+      .then((data) => data.message);
+
+    return response;
   };
 
   const mutation = useMutation({
     mutationFn: postReport,
     onMutate: () => setPosted(true),
+    onSuccess: (response) => {
+      setMessage(response);
+      setPosted(false);
+    },
   });
 
   return (
     <div>
+      <input ref={titleRef} />
+      <input ref={bodyRef} />
       {!isPosted && <button onClick={() => mutation.mutate()}>送信</button>}
       {mutation.isPending && <LoadingSpinner />}
+      <h3>{message}</h3>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import HeaderComponent from "./StyledComponents/HeaderComponent";
 import { Heading1 } from "./StyledComponents/StyledHeading";
 
-const Header = () => (
-  <HeaderComponent>
-    <Heading1>グリーンバック動画合成</Heading1>
-  </HeaderComponent>
-);
+const Header = () => {
+  const router = useRouter();
+
+  return (
+    <HeaderComponent>
+      <Heading1>グリーンバック動画合成</Heading1>
+      <p onClick={() => router.push("/user_report_form")}>不具合報告はこちら</p>
+    </HeaderComponent>
+  );
+};
 
 export default Header;
