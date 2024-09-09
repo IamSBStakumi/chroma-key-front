f95ab793484a3327c7e1e4d84e93b3a2de34dbbc
kuri884 <krc.kuribayashi@gmail.com>
2024-09-09 16:43:57 +0900

    issue作成用のサーバーサイドを作成

---
+++ src/app/api/report/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({ auth: process.env.NEXT_PUBLIC_GITHUB_TOKEN });
+
+export async function POST(req: NextRequest) {
+  try {
+    const { title, body } = await req.json();
+
+    const status: number = await octokit
+      .request("POST /repos/IamSBStakumi/chroma-key-front/issues", {
+        owner: "IamSBStakumi",
+        repo: "chroma-key-front",
+        title: title,
+        body: body,
+      })
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .then((res: any) => res?.data?.status);
+
+    if (status === 200) return NextResponse.json({ message: "不具合を報告いただきまして、ありがとうございます。" });
+
+    return NextResponse.json({ message: "申し訳ありませんが、投稿できませんでした。" });
+  } catch (error: unknown) {
+    console.error("Error is occurred:", error);
+
+    return NextResponse.json({ message: "Error:", error }, { status: 500 });
+  }
+}
