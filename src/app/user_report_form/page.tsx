98dfa65b0e4328fbd19a6d2859790b43bc372569
kuri884 <krc.kuribayashi@gmail.com>
2024-09-10 13:51:19 +0900

    issue投稿ができた時、戻るボタンを表示するように変更

--- src/app/user_report_form/page.tsx
+++ src/app/user_report_form/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useRef } from "react";
+import { useRouter } from "next/navigation";
 import { useMutation } from "@tanstack/react-query";
 import { ReportFormWrapper } from "@/components/StyledComponents/WrapperComponents";
 import { PageTitle } from "@/components/StyledComponents/StyledHeading";
@@ -10,9 +11,11 @@ import LoadingSpinner from "@/components/LoadingSpinner";
 
 const UserReportForm = () => {
   const [isPosted, setPosted] = useState(false);
+  const [isSuccess, setSuccess] = useState(false);
   const [message, setMessage] = useState("");
   const titleRef = useRef<HTMLInputElement>(null);
   const bodyRef = useRef<HTMLTextAreaElement>(null);
+  const router = useRouter();
 
   const postReport = async () => {
     if (!titleRef.current || !bodyRef.current) return;
@@ -22,11 +25,11 @@ const UserReportForm = () => {
     const response = await fetch("/api/report", {
       method: "POST",
       body: JSON.stringify({ title: titleRef.current.value, body: bodyRef.current.value }),
-    })
-      .then(async (res) => await res.json())
-      .then((data) => data.message);
+    }).then(async (res) => await res.json());
 
-    return response;
+    setSuccess(response.isSuccess);
+
+    return response.message;
   };
 
   const mutation = useMutation({
@@ -43,9 +46,10 @@ const UserReportForm = () => {
       <PageTitle>不具合報告ページ</PageTitle>
       <InputField labelName="概要" inputRef={titleRef} id="title" />
       <InputField labelName="不具合の内容" inputRef={bodyRef} id="body" />
-      {!isPosted && <Button onClick={() => mutation.mutate()}>送信</Button>}
+      {!isPosted && !isSuccess && <Button onClick={() => mutation.mutate()}>送信</Button>}
       {mutation.isPending && <LoadingSpinner />}
       <p>{message}</p>
+      {isSuccess && <Button onClick={() => router.push("/")}>戻る</Button>}
     </ReportFormWrapper>
   );
 };
