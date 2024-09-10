7a372bd746092df0ac6693fdfdb13e6ee2c8f56d
kuri884 <krc.kuribayashi@gmail.com>
2024-09-10 14:13:56 +0900

    placeholderを指定

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -15,9 +15,9 @@ const InputField = ({ labelName, inputRef, id }: Props) => {
         <span> *</span>
       </LabelText>
       {labelName === "不具合の内容" ? (
-        <BodyForm ref={inputRef as RefObject<HTMLTextAreaElement>} id={id} />
+        <BodyForm ref={inputRef as RefObject<HTMLTextAreaElement>} id={id} placeholder="要望などもこちらへどうぞ" />
       ) : (
-        <TitleForm ref={inputRef as RefObject<HTMLInputElement>} id={id} />
+        <TitleForm ref={inputRef as RefObject<HTMLInputElement>} id={id} placeholder="適当なタイトルを付けてください" />
       )}
     </>
   );
