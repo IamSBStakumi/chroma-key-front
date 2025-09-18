3d9c9ffa778e6ee00bc4ad01483c9b0ce0180486
kuri884 <krc.kuribayashi@gmail.com>
2025-09-18 14:52:22 +0900

    フォルダ構成を編集し、StyledComponentsRegistryをnonce対応

--- src/lib/styled-components/registry.tsx
+++ src/lib/styled-components/registry.tsx
@@ -1,18 +1,26 @@
 "use client";
 
+import useNonceFix from "@/hooks/useNonceFix";
 import { useServerInsertedHTML } from "next/navigation";
 import React, { useState } from "react";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 
-const StyledComponentsRegistry = ({ children }: { children: React.ReactNode }) => {
+const StyledComponentsRegistry = ({ children, nonce = "" }: { children: React.ReactNode; nonce?: string }) => {
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
 
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement();
-    styledComponentsStyleSheet.instance.clearTag();
+    const styled = React.Children.map(styles, (child) =>
+      React.isValidElement(child)
+        ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          React.cloneElement(child as React.ReactElement<unknown>, { nonce } as any)
+        : child,
+    );
 
-    return <>{styles}</>;
+    return styled;
   });
+
+  useNonceFix(nonce);
 
   if (typeof window !== "undefined") return <>{children}</>;
 
